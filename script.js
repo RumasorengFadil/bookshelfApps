a41bc0d5640bfca775e2b094d95f1a20698172ea
Fadil Hijayat <limitvariabel@gmail.com>
2024-04-11 08:49:34 +0900

    Success completed the book application

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const submitBtn = document.querySelector(".form__submit");
 const todoEl = document.querySelector(".todo");
 const searchInput = document.querySelector(".search__input-search");
 const overlay = document.querySelector(".overlay");
-const btnClose = document.querySelector(".form__btn-close");
+const btnX = document.querySelector(".form__btn-close");
+const popupDelete = document.querySelector(".popup");
+const btnDelete = document.querySelector(".popup__btn-delete");
+const btnCancel = document.querySelector(".popup__btn-cancel");
 let inputTitle = document.querySelector("#inputJudul");
 let inputWriter = document.querySelector("#inputPenulis");
 let inputYear = document.querySelector("#inputTahun");
@@ -16,6 +19,10 @@ let isCompleted = document.querySelector("#inputStatus");
 let todoList = [];
 
 //todo Function
+const createLocalStorage = function(value){
+    if(localStorage.getItem(LOCAL_STR_KEY)) return;
+    localStorage.setItem(LOCAL_STR_KEY,JSON.stringify(value));
+}
 const renderTodo = function(todo){
     const todoMarkup = `
     <div class="card" id=${todo.id}>
@@ -32,120 +39,67 @@ const renderTodo = function(todo){
     completedTodo.insertAdjacentHTML("beforeend", todoMarkup)
     :uncompletedTodo.insertAdjacentHTML("beforeend", todoMarkup);
 }
-
-const clearTodo = function(){
-    completedTodo.innerHTML = "";
-    uncompletedTodo.innerHTML = "";
-}
-
-
-const createLocalStorage = function(){
-    if(localStorage.getItem(LOCAL_STR_KEY)) return;
-
-    localStorage.setItem(LOCAL_STR_KEY,JSON.stringify([]));
-}
-
-const update = function(todo){
-    localStorage.setItem(LOCAL_STR_KEY,JSON.stringify(todo));
-}
-
 const generateId = function(){
     return todoList[todoList.length - 1]? 
     todoList[todoList.length - 1].id+1 
     : 0;
 }
-
-const toggleTodoList = function(){
-    const cardId = +this.target.closest(".card").id;
-    const todoIndex = todoList.findIndex(todo => todo.id === cardId);
-
-    todoList[todoIndex].isCompleted =  !todoList[todoIndex].isCompleted;
-
-    clearTodo();
-    todoList.forEach(todo => renderTodo(todo));
-}
-
-const deleteTodo = function(){
-    const cardId = +this.target.closest(".card").id;
-        
-    todoList = todoList.filter(todo =>{
-        return todo.id !== cardId;
-    })
-
-    update(todoList);
-    clearTodo();
-    todoList.forEach(todo => renderTodo(todo));
-}
 const toggleEditForm = function(){
     form.classList.toggle("form-edit");
     overlay.classList.toggle("hidden");
-    btnClose.classList.toggle("hidden");
+    btnX.classList.toggle("hidden");
     submitBtn.value = "Masukan ke rak";
 }
-
+const update = function(todo){
+    localStorage.setItem(LOCAL_STR_KEY,JSON.stringify(todo));
+}
+const clearTodo = function(){
+    completedTodo.innerHTML = "";
+    uncompletedTodo.innerHTML = "";
+}
 const clearForm = function(){
     inputTitle.value = "";
     inputWriter.value = "";
     inputYear.value = "";
     isCompleted.checked = false;
 }
+const toggleTodoList = function(){
+    const cardId = +form.id;
+    const todoIndex = todoList.findIndex(todo => todo.id === cardId);
+
+    todoList[todoIndex].isCompleted =  !todoList[todoIndex].isCompleted;
+
+    update(todoList);
+    clearTodo();
+    todoList.forEach(todo => renderTodo(todo));
+}
+const toggleDeletePopup = function(){
+    popupDelete.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+}
+const deleteTodo = function(){
+    const cardId = +form.id;
+    console.log(form.id);
+        
+    todoList = todoList.filter(todo =>{
+        console.log(todo.id !== cardId)
+        return todo.id !== cardId;
+    })
+
+    update(todoList);
+    toggleDeletePopup();
+    clearTodo();
+    todoList.forEach(todo => renderTodo(todo));
+}
 
 // todo Event
-searchInput.addEventListener("input", function(){
-    const todoListEl = document.querySelectorAll(".card");
-    const searchValue = searchInput.value.toLowerCase();
+document.addEventListener("DOMContentLoaded", function(){
+    createLocalStorage([]);
 
-    todoListEl.forEach(todo => {
-        const todoTitle = todo.querySelector(".card__judul").textContent.toLowerCase();
-        const todoWriter = todo.querySelector(".card__penulis").textContent.toLowerCase();
-        const todoYear = todo.querySelector(".card__tahun").textContent.toLowerCase();
-        
-        if(todoTitle.includes(searchValue) || 
-        todoWriter.includes(searchValue) ||
-        todoYear.includes(searchValue)){
-            todo.classList.remove("hidden");
-        }else{
-            todo.classList.add("hidden");
-        }
-    })
+    todoList = JSON.parse(localStorage.getItem(LOCAL_STR_KEY));
+
+    todoList.forEach(todo => renderTodo(todo));
 })
-
-todoEl.addEventListener("click", function(e){
-    if(e.target.classList.contains("card__btn-pindah")){
-        toggleTodoList.call(e);
-    }
-
-    if(e.target.classList.contains("card__btn-delete")){
-        deleteTodo.call(e);
-    }
-
-    if(e.target.classList.contains("card__icn-edit")){
-        toggleEditForm();
-
-        const cardId = +e.target.closest(".card").id;
-        const todo = todoList
-        .find(todo => todo.id === cardId);
-
-        inputTitle.value = todo.titleValue;
-        inputWriter.value = todo.writerValue;
-        inputYear.value = todo.yearValue;
-        isCompleted.checked = todo.isCompleted;
-        form.id = todo.id;
-        
-        submitBtn.value = "Ubah";
-    }
-})
-
-overlay.addEventListener("click", _=> {
-    toggleEditForm();
-    clearForm();
-});
-
-btnClose.addEventListener("click", _=> {
-    toggleEditForm();
-    clearForm();
-});
-
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
@@ -165,6 +119,7 @@ form.addEventListener("submit", function(e){
     }else{
         todoList.push(todoObj);
     }
+
     update(todoList);
     
     clearTodo();
@@ -172,11 +127,54 @@ form.addEventListener("submit", function(e){
 
     clearForm();
 })
+searchInput.addEventListener("input", function(){
+    const todoListEl = document.querySelectorAll(".card");
+    const searchValue = searchInput.value.toLowerCase();
 
-document.addEventListener("DOMContentLoaded", function(){
-    createLocalStorage();
-
-    todoList = JSON.parse(localStorage.getItem(LOCAL_STR_KEY));
-
-    todoList.forEach(todo => renderTodo(todo));
+    todoListEl.forEach(todo => {
+        const todoTitle = todo.querySelector(".card__judul").textContent.toLowerCase();
+        const todoWriter = todo.querySelector(".card__penulis").textContent.toLowerCase();
+        const todoYear = todo.querySelector(".card__tahun").textContent.toLowerCase();
+        
+        if(todoTitle.includes(searchValue) || 
+        todoWriter.includes(searchValue) ||
+        todoYear.includes(searchValue)){
+            todo.classList.remove("hidden");
+        }else{
+            todo.classList.add("hidden");
+        }
+    })
 })
+todoEl.addEventListener("click", function(e){
+    form.id = e.target.closest(".card").id;
+
+    if(e.target.classList.contains("card__btn-pindah")){
+        toggleTodoList();
+    }
+
+    if(e.target.classList.contains("card__btn-delete")){
+        toggleDeletePopup();
+    }
+
+    if(e.target.classList.contains("card__icn-edit")){
+        toggleEditForm();
+
+        const cardId = +e.target.closest(".card").id;
+        const todo = todoList
+        .find(todo => todo.id === cardId);
+
+        inputTitle.value = todo.titleValue;
+        inputWriter.value = todo.writerValue;
+        inputYear.value = todo.yearValue;
+        isCompleted.checked = todo.isCompleted;
+        form.id = todo.id;
+        
+        submitBtn.value = "Ubah";
+    }
+})
+btnX.addEventListener("click", function(){
+    toggleEditForm();
+    clearForm();
+});
+btnCancel.addEventListener("click",toggleDeletePopup);
+btnDelete.addEventListener("click", deleteTodo);
